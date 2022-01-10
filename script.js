3b1ca03eb93f62d9fe583709d2cca14af413c6b0
pan-cynthia <cypan@ucdavis.edu>
2022-01-09 20:21:21 -0800

    worked on design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const pokemonContainer = document.getElementById('pokemon-container');
+const pokedex = document.getElementById('pokedex');
 const numOfPokemon = 150;
 
-// map types to colors
+// type colors
 const colors = {
   normal: '#A8A878',
   fighting: '#C03028',
@@ -24,6 +24,29 @@ const colors = {
   unknown: '#68A090'
 };
 
+// background colors
+const backgroundColors = {
+  normal: 'rgba(168, 168, 120, 0.5)',
+  fighting: 'rgba(192, 48, 40, 0.5)',
+  flying: 'rgba(168, 144, 240, 0.5)',
+  poison: 'rgba(160, 64, 160, 0.5)',
+  ground: 'rgba(224, 192, 104, 0.5)',
+  bug: 'rgba(168, 184, 32, 0.5)',
+  ghost: 'rgba(112, 88, 152, 0.5)',
+  rock: 'rgba(184, 160, 56, 0.5)',
+  steel: 'rgba(184, 184, 208, 0.5)',
+  fire: 'rgba(240, 128, 48, 0.5)',
+  water: 'rgba(104, 144, 240, 0.5)',
+  grass: 'rgba(120, 200, 80, 0.5)',
+  electric: 'rgba(248, 208, 48, 0.5)',
+  psychic: 'rgba(248, 88, 136, 0.5)',
+  ice: 'rgba(152, 216, 216, 0.5)',
+  dragon: 'rgba(112, 56, 248, 0.5)',
+  dark: 'rgba(112, 88, 72, 0.5)',
+  fairy: 'rgba(238, 153, 172, 0.5)',
+  unknown: 'rgba(104, 160, 144, 0.5)'
+}
+
 const createPokemonCard = (pokemon) => {
   // extract metadata from pokemon json object
   let {id, name, types, sprites} = pokemon;
@@ -31,7 +54,7 @@ const createPokemonCard = (pokemon) => {
   (id < 10) ? id = '00' + id : (id < 100) ? id = '0' + id : id;
   // create a new div element that will hold all of the pokemon's information
   const pokemonCard = document.createElement('div');
-  // make its class 'pokemon'
+  // make its class 'pokemon-card'
   pokemonCard.classList.add('pokemon-card');
   if (types[1] !== undefined) {
     // pokemon has a secondary type
@@ -48,7 +71,8 @@ const createPokemonCard = (pokemon) => {
       </div>
     </div>
     `;
-    pokemonCard.style.background = `linear-gradient(to right, ${colors[types[0].type.name]}, ${colors[types[1].type.name]})`;
+    // use both type colors in background of pokemon card
+    pokemonCard.style.background = `linear-gradient(to right, ${backgroundColors[types[0].type.name]}, ${backgroundColors[types[1].type.name]})`;
   } else {
     // pokemon only has a primary type
     pokemonCard.innerHTML = `
@@ -63,10 +87,22 @@ const createPokemonCard = (pokemon) => {
       </div>
     </div>
     `;
-    pokemonCard.style.background = colors[types[0].type.name];
+    // set background color of pokemon card to the color of its type
+    pokemonCard.style.background = backgroundColors[types[0].type.name];
   }
   // add the new element into the pokemon container element
-  pokemonContainer.appendChild(pokemonCard);
+  pokedex.appendChild(pokemonCard);
+  // set background colors types on card
+  let primaryTypes = document.getElementsByClassName('primary-type');
+  primaryTypes[primaryTypes.length - 1].style.background = colors[types[0].type.name];
+  if (types[1] !== undefined) {
+    let secondaryTypes = document.getElementsByClassName('secondary-type');
+    secondaryTypes[secondaryTypes.length - 1].style.background = colors[types[1].type.name];
+  }
+  // if user clicks on a specific pokemon card, redirect to a new page that will display more info about that pokemon
+  pokemonCard.addEventListener('click', () => {
+    displayInfo(pokemon);
+  })
 }
 
 // makes an api call to get a single pokemon using its id number
@@ -75,6 +111,7 @@ const getPokemon = async id => {
   const response = await fetch(url);
   const pokemon = await response.json();
   createPokemonCard(pokemon);
+  
 }
 
 // get all pokemon
@@ -85,3 +122,7 @@ const fetchPokemon = async () => {
 }
 
 fetchPokemon();
+
+function displayInfo(pokemon) {
+  console.log(pokemon);
+}
